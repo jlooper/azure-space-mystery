f8d32755d94cefc4aedc8b76dc634c40d425756a
Jen Looper <jen.looper@gmail.com>
2020-12-11 16:58:15 -0500

    removing style purge

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	prefix: '',
 	important: false,
-	purge: ['./src/**/*.html', './src/**/*.vue'],
 	separator: ':',
 	theme: {
 		screens: {
@@ -11,14 +10,6 @@ module.exports = {
 			xl: '1280px',
 		},
 		colors: {
-			'smoke-darkest': 'rgba(0, 0, 0, 0.9)',
-			'smoke-darker': 'rgba(0, 0, 0, 0.75)',
-			'smoke-dark': 'rgba(0, 0, 0, 0.6)',
-			smoke: 'rgba(0, 0, 0, 0.5)',
-			'smoke-light': 'rgba(0, 0, 0, 0.4)',
-			'smoke-lighter': 'rgba(0, 0, 0, 0.25)',
-			'smoke-lightest': 'rgba(0, 0, 0, 0.1)',
-
 			transparent: 'transparent',
 
 			black: '#000',
